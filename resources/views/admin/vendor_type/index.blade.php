5a988e22ad0baf19f96b66e2e2adef1b21a22327
vishalnv21 <vishal.nv21@gmail.com>
2023-09-21 14:48:01 +0530

    bug

--- resources/views/admin/vendor_type/index.blade.php
+++ resources/views/admin/vendor_type/index.blade.php
@@ -46,11 +46,11 @@
                                                 </div>
                                             </td>
                                             <td>
-                                                <a type="button" id="actionEditBtn_{{$data->id}}" data-bs-toggle="dropdown"
+                                                <a type="button" id="ActionBtn_{{$data->id}}" data-bs-toggle="dropdown" role="button"
                                                     aria-haspopup="true" aria-expanded="false">
                                                     <i class="bi bi-three-dots-vertical icon-lg text-muted pb-3px"></i>
                                                 </a>
-                                                <div class="dropdown-menu" aria-labelledby="actionEditBtn_{{$data->id}}">
+                                                <div class="dropdown-menu" aria-labelledby="ActionBtn_{{$data->id}}">
                                                     <a class="dropdown-item d-flex align-items-center" href=""><i
                                                             class="bi bi-eye icon-sm me-2"></i><span>View</span></a>
                                                     <a href="javascript:;" class="dropdown-item d-flex align-items-center" wire:click="edit({{$data->id}})"><i
