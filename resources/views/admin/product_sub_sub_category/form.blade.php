015ef66f3b96661cb6dc68b1ab1ade578e27722a
vishalnv21 <vishal.nv21@gmail.com>
2023-09-26 14:02:53 +0530

    product sub sub category redesigned

--- resources/views/admin/product_sub_sub_category/form.blade.php
+++ resources/views/admin/product_sub_sub_category/form.blade.php
@@ -15,103 +15,105 @@
                 <div class="card-body">
                     <form class="forms-sample" wire:submit.prevent="{{$hidden_id ? 'update()' : 'save()'}}">
                         <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="business-category" class="form-label">Business Category</label>
-                                <select class="form-select mb-4 mb-md-0 @error('business_category_id') is-invalid @enderror" id="business-category"  wire:model.defer="business_category_id" wire:change="setProductCategoryList()">
-                                    <option selected="">Select Business Category</option>
-                                    @foreach($business_category_list as $business_category)
-                                    <option value="{{$business_category->id}}">{{$business_category->name}}</option>
-                                    @endforeach
-                                </select>
-                                @error('business_category_id') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                            <div class="col-md-6">
-                                <label for="business-product-category" class="form-label">Product Category</label>
-                                <select class="form-select @error('product_category_id') is-invalid @enderror" id="business-product-category"  wire:model.defer="product_category_id" wire:change="setProductSubCategoryList()">
-                                    <option selected="">Select Product Category</option>
-                                    @foreach($product_category_list as $product_category)
-                                    <option value="{{$product_category->id}}">{{$product_category->name}}</option>
-                                    @endforeach
-                                </select>
-                                @error('product_category_id') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label for="product-sub-category" class="form-label">Product Sub Category</label>
-                                <select class="form-select @error('product_sub_category_id') is-invalid @enderror" id="product-sub-category" wire:model.defer="product_sub_category_id">
-                                    <option selected>Select Product Sub Category</option>
-                                    @foreach ($product_sub_category_list as $product_sub_category)
-                                        <option value="{{$product_sub_category->id}}">{{$product_sub_category->name}}</option>
-                                    @endforeach
-                                </select>
-                                @error('product_sub_category_id') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                            <div class="col-md-6">
-                                <label class="form-label mt-3 mt-md-0" for="name">Name</label>
-                                <input type="text" id="name" class="form-control mb-3 mb-md-0 @error('name') is-invalid @enderror" wire:model.defer="name">
-                                @error('name') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label class="form-label" for="icon">Icon</label>
-                                <div class="input-group">
-                                    <span class="input-group-text">{!! $icon ? $icon : '<i class="fa fa-circle-o" aria-hidden="true"></i>'!!}</span>
-                                    <input type="text" id="icon" class="form-control @error('icon') is-invalid @enderror" wire:model.defer="icon">
+                            <div class="col-md-8 border-end">
+                                <div class="row">
+                                    <h5 class="card-heading-h5">Product Sub SubCategory Details:</h5>
+                                    <div class="col-md-12 mb-3">
+                                        <label for="business-category" class="form-label">Business Category</label>
+                                        <select class="form-select @error('business_category_id') is-invalid @enderror" id="business-category"  wire:model.defer="business_category_id" wire:change="setProductCategoryList()">
+                                            <option selected="">Select Business Category</option>
+                                            @foreach($business_category_list as $business_category)
+                                            <option value="{{$business_category->id}}">{{$business_category->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('business_category_id') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="business-product-category" class="form-label">Product Category</label>
+                                        <select class="form-select @error('product_category_id') is-invalid @enderror" id="business-product-category"  wire:model.defer="product_category_id" wire:change="setProductSubCategoryList()">
+                                            <option selected="">Select Product Category</option>
+                                            @foreach($product_category_list as $product_category)
+                                            <option value="{{$product_category->id}}">{{$product_category->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('product_category_id') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="product-sub-category" class="form-label">Product Sub Category</label>
+                                        <select class="form-select @error('product_sub_category_id') is-invalid @enderror" id="product-sub-category" wire:model.defer="product_sub_category_id">
+                                            <option selected>Select Product Sub Category</option>
+                                            @foreach ($product_sub_category_list as $product_sub_category)
+                                                <option value="{{$product_sub_category->id}}">{{$product_sub_category->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('product_sub_category_id') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label class="form-label mt-3 mt-md-0" for="name">Name</label>
+                                        <input type="text" id="name" class="form-control @error('name') is-invalid @enderror" wire:model.defer="name" placeholder="Enter name">
+                                        @error('name') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label class="form-label" for="icon">Icon</label>
+                                        <div class="input-group">
+                                            <span class="input-group-text">{!! $icon ? $icon : '<i class="fa fa-circle-o" aria-hidden="true"></i>'!!}</span>
+                                            <input type="text" id="icon" class="form-control @error('icon') is-invalid @enderror" wire:model.defer="icon" placeholder="Enter icon">
+                                        </div>
+                                        @error('icon') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <h5 class="card-heading-h5">SEO Section:</h5>
+                                    <div class="col-md-6 mb-3">
+                                        <label class="form-label" for="title">Meta Title</label>
+                                        <input type='text' id="title" class="form-control @error('meta_title') is-invalid @enderror" wire:model.defer='meta_title' placeholder="Enter title">
+                                        @error('meta_title') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label class="form-label" for="keyword">Meta Keywords</label>
+                                        <input type='text' id="keyword" class="form-control @error('meta_keywords') is-invalid @enderror" wire:model.defer='meta_keywords' placeholder="Enter keywords">
+                                        @error('meta_keywords') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-12 mb-3">
+                                        <label for="FormControlTextarea" class="form-label">Meta Description</label>
+                                        <textarea class="form-control @error('meta_description') is-invalid @enderror" id="FormControlTextarea" rows="5" wire:model.defer='meta_description' placeholder="Enter description"></textarea>
+                                        @error('meta_description') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
                                 </div>
-                                @error('icon') <small class="text-danger">{{ $message }}</small>@enderror
+                            </div>
+                            <div class="col-md-4 border-start">
+                                <div class="row">
+                                    <h5 class="card-heading-h5">Images:</h5>
+                                    <div class="col-md-12">
+                                        <label class="form-label">Thumbnail Image</label>
+                                        <input type='file' id="product_subsubcategory_thumbnail" class="form-control @error('thumbnail') is-invalid @enderror" wire:model.defer="thumbnail">
+                                        <label for="product_subsubcategory_thumbnail">
+                                            @if($thumbnail)
+                                                <img src="{{$thumbnail->temporaryUrl()}}" class="label-thumbnail">
+                                            @elseif ($showThumbnail)
+                                                <img src="{{asset('storage/'.$showThumbnail)}}" class="label-thumbnail">
+                                            @else
+                                                <img class="label-thumbnail" src="{{asset('admin_css/assets/images/others/placeholder.jpg')}}" >
+                                            @endif
+                                        </label>
+                                        @error('thumbnail') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label class="form-label">Banner Image</label>
+                                        <input type='file' id="product_subsubcategory_banner" class="form-control @error('banner') is-invalid @enderror" wire:model.defer="banner">
+                                        <label for="product_subsubcategory_banner">
+                                            @if($banner)
+                                                <img src="{{$banner->temporaryUrl()}}" class="label-banner">
+                                            @elseif ($showBanner)
+                                                <img src="{{asset('storage/'.$showBanner)}}" class="label-banner">
+                                            @else
+                                                <img class="label-thumbnail" src="{{asset('admin_css/assets/images/others/placeholder.jpg')}}" >
+                                            @endif
+                                        </label>
+                                        @error('banner') <small class="text-danger">{{ $message }}</small>@enderror
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <label class="form-label">Thumbnail Image</label>
-                                <input type='file' id="product_subsubcategory_thumbnail" class="form-control @error('thumbnail') is-invalid @enderror" wire:model.defer="thumbnail">
-                                <label for="product_subsubcategory_thumbnail">
-                                    @if($thumbnail)
-                                        <img src="{{$thumbnail->temporaryUrl()}}" class="label-thumbnail">
-                                    @elseif ($showThumbnail)
-                                        <img src="{{asset('storage/'.$showThumbnail)}}" class="label-thumbnail">
-                                    @else
-                                        <img class="label-thumbnail" src="{{asset('admin_css/assets/images/others/placeholder.jpg')}}" >
-                                    @endif
-                                </label>
-                                @error('thumbnail') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                            <div class="col-md-6">
-                                <label class="form-label">Banner Image</label>
-                                <input type='file' id="product_subsubcategory_banner" class="form-control @error('banner') is-invalid @enderror" wire:model.defer="banner">
-                                <label for="product_subsubcategory_banner">
-                                    @if($banner)
-                                        <img src="{{$banner->temporaryUrl()}}" class="label-banner">
-                                    @elseif ($showBanner)
-                                        <img src="{{asset('storage/'.$showBanner)}}" class="label-banner">
-                                    @else
-                                        <img class="label-thumbnail" src="{{asset('admin_css/assets/images/others/placeholder.jpg')}}" >
-                                    @endif
-                                </label>
-                                @error('banner') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <label class="form-label" for="title">Meta Title</label>
-                                <input type='text' id="title" class="form-control mb-3 mb-md-0 @error('meta_title') is-invalid @enderror" wire:model.defer='meta_title'>
-                                @error('meta_title') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                            <div class="col-md-6">
-                                <label class="form-label" for="keyword">Meta Keywords</label>
-                                <input type='text' id="keyword" class="form-control mb-3 mb-md-0 @error('meta_keywords') is-invalid @enderror" wire:model.defer='meta_keywords'>
-                                @error('meta_keywords') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <div class="col-md-12">
-                                <label for="FormControlTextarea" class="form-label">Meta Description</label>
-                                <textarea class="form-control @error('meta_description') is-invalid @enderror" id="FormControlTextarea" rows="5" wire:model.defer='meta_description'></textarea>
-                                @error('meta_description') <small class="text-danger">{{ $message }}</small>@enderror
-                            </div>
-                        </div>
+
                         <div class="row">
                             <div class="col-md-6">
                                 <x-submit-btn text="{{$hidden_id?'Update':'Save'}}" />
