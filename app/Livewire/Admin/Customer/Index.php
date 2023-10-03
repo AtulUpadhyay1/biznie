bbb7150c77310373db464f5095de4c028d418c36
vishalnv21 <vishal.nv21@gmail.com>
2023-10-03 17:00:05 +0530

    Slider changed

--- app/Livewire/Admin/Customer/Index.php
+++ app/Livewire/Admin/Customer/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('admin.customer_list.index', ['page_title' => 'Customer List']);
+        return view('admin.customer_list.index', ['page_title' => 'All Customers']);
     }
 }
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -18,165 +18,218 @@
                     <span class="link-title">Dashboard</span>
                 </a>
             </li>
+
+            <!--Sellers-->
             <li class="nav-item">
-                <a href="{{route('admin.customers')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="users"></i>
-                    <span class="link-title">All Customers</span>
+                <a class="nav-link" data-bs-toggle="collapse" href="#sellers" role="button"
+                    aria-expanded="false" aria-controls="sellers">
+                    <i class="link-icon" data-feather="user-plus"></i>
+                    <span class="link-title">Sellers</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
             </li>
+            <div class="collapse" id="sellers">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="{{route('admin.seller-list')}}" class="nav-link" wire:navigate>
+                           All Sellers
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.business-listing')}}" class="nav-link" wire:navigate>
+                           Business Listings
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!--End of seller -->
+
+            <!--Customers-->
             <li class="nav-item">
-                <a href="{{route('admin.seller-list')}}" class="nav-link" wire:navigate>
+                <a class="nav-link" data-bs-toggle="collapse" href="#customer" role="button"
+                    aria-expanded="false" aria-controls="customer">
                     <i class="link-icon" data-feather="users"></i>
-                    <span class="link-title">All Sellers</span>
+                    <span class="link-title">Customers</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
             </li>
-            <li class="nav-item">
-                <a href="{{route('admin.business-listing')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="users"></i>
-                    <span class="link-title">Business Listings</span>
-                </a>
-            </li>
+            <div class="collapse" id="customer">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="{{route('admin.customers')}}" class="nav-link" wire:navigate>
+                            All Customers
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!--End of customer -->
+
             <!--products-->
-            {{-- <li class="nav-item">
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#products" role="button"
+                    aria-expanded="false" aria-controls="products">
+                    <i class="link-icon" data-feather="shopping-cart"></i>
+                    <span class="link-title">Products</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+            </li>
+            <div class="collapse" id="products">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Add New Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">All Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.product-category')}}" class="nav-link" wire:navigate>
+                            Category
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.product-sub-category')}}" class="nav-link" wire:navigate>
+                            Sub Category
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.product-sub-subcategory')}}" class="nav-link" wire:navigate>
+                            Sub Sub Category
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+            <!--End of product -->
+
+            <!--Businesses-->
+            <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#business" role="button"
                     aria-expanded="false" aria-controls="business">
                     <i class="link-icon" data-feather="shopping-bag"></i>
-                    <span class="link-title">Products</span>
+                    <span class="link-title">Business</span>
                     <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
             </li>
             <div class="collapse" id="business">
                 <ul class="nav sub-menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">All Products</a>
+                        <a href="#" class="nav-link">
+                            Order Book
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Add Products</a>
+                        <a href="#" class="nav-link">
+                            Invoice
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Payment List
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Analytics
+                        </a>
                     </li>
                 </ul>
-            </div> --}}
-            <li class="nav-item">
-                <a href="{{route('admin.product-category')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Category</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.product-sub-category')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Sub Category</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.product-sub-subcategory')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Sub Sub Category</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="plus-circle"></i>
-                    <span class="link-title">All Products</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="plus-circle"></i>
-                    <span class="link-title">Order Book</span>
-                </a>
-            </li>
+            </div>
+            <!--End of business -->
 
-            <!--Businesses-->
+            <!--Messages-->
             <li class="nav-item">
-                <a href="{{route('admin.business-category')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="inbox"></i>
-                    <span class="link-title">Business Category</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.vendor-type')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="user-plus"></i>
-                    <span class="link-title">Vendor Type</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.seller-type')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="shopping-bag"></i>
-                    <span class="link-title">Seller Type</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.gst-type')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">GST Type</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.product-unit')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Unit</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.tax-type')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="pie-chart"></i>
-                    <span class="link-title">Tax Type</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('admin.identity-type')}}" class="nav-link" wire:navigate>
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Identity Type</span>
-                </a>
-            </li>
-
-            <!--Transaction-->
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="calendar"></i>
-                    <span class="link-title">Invoice</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="credit-card"></i>
-                    <span class="link-title">Payment List</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="aperture"></i>
-                    <span class="link-title">Analytics</span>
-                </a>
-            </li>
-
-            <!--Inboxes-->
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="compass"></i>
-                    <span class="link-title">Request For Quotation</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a class="nav-link" data-bs-toggle="collapse" href="#message" role="button"
+                    aria-expanded="false" aria-controls="message">
                     <i class="link-icon" data-feather="message-square"></i>
                     <span class="link-title">Messages</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
             </li>
+            <div class="collapse" id="message">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                           Request For Quotation
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                           Messages
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!--End of messages-->
 
-            <!--Shop Setup-->
+            <!--Biznie Set up-->
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="tool"></i>
-                    <span class="link-title">Shop Setup</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a class="nav-link" data-bs-toggle="collapse" href="#master-setting" role="button"
+                    aria-expanded="false" aria-controls="master-setting">
                     <i class="link-icon" data-feather="settings"></i>
-                    <span class="link-title">Setting</span>
+                    <span class="link-title">Biznie Setup</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
             </li>
+            <div class="collapse" id="master-setting">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="{{route('admin.business-category')}}" class="nav-link" wire:navigate>
+                           Business Category
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.vendor-type')}}" class="nav-link" wire:navigate>
+                            Vendor Type
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.seller-type')}}" class="nav-link" wire:navigate>
+                           Seller Type
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.gst-type')}}" class="nav-link" wire:navigate>
+                           GST Type
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.product-unit')}}" class="nav-link" wire:navigate>
+                           Unit
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.tax-type')}}" class="nav-link" wire:navigate>
+                           Tax Type
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('admin.identity-type')}}" class="nav-link" wire:navigate>
+                            Identity Type
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- End of biznie set up -->
+
+            <!--App Setup-->
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#app-setup" role="button"
+                    aria-expanded="false" aria-controls="app-setup">
+                    <i class="link-icon" data-feather="tool"></i>
+                    <span class="link-title">App Setup</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+            </li>
+            <div class="collapse" id="app-setup">
+                <ul class="nav sub-menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                           App Banner/Sliders</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- End of app setup -->
 
             <!--Others-->
             <li class="nav-item">
