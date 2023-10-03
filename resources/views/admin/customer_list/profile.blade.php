feb6d868d0431e78c187b34a95a49ad240b82ab5
vishalnv21 <vishal.nv21@gmail.com>
2023-10-03 18:31:40 +0530

    Customer Profile

--- resources/views/admin/customer_list/profile.blade.php
+++ resources/views/admin/customer_list/profile.blade.php
@@ -1,3 +1,45 @@
 <div>
-    hello
+    <div class="row">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card position-sticky fixed-top">
+                        <div class="card-header">
+                            <div class="text-center">
+                                <img src="{{asset('admin_css/assets/images/avatar.png')}}" alt="" class="w-25 h-25">
+                                <h6 class="text-dark mt-3">Sushanshu Kumar</h6>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <ul class="custom-un-li">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Customer Info
+                                        <i class="bi bi-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Order Details
+                                        <i class="bi bi-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">Payment History
+                                        <i class="bi bi-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">RFQ
+                                        <i class="bi bi-chevron-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
