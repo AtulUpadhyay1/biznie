bdfa69b4736b5bd1685bfc337724da1b9536c936
vishalnv21 <vishal.nv21@gmail.com>
2023-10-05 16:14:30 +0530

    Product edit livewire created

--- app/Livewire/Admin/Product/Index.php
+++ app/Livewire/Admin/Product/Index.php
@@ -11,6 +11,6 @@ class Index extends Component
 
     public function render()
     {
-        return view('admin.products.index', ['page_title' => 'Product List']);
+        return view('admin.product.index', ['page_title' => 'Product List']);
     }
 }
