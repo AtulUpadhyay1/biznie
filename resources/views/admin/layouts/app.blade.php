b0361e32101794d2ee62563794e3bdfaef46f8d9
vishalnv21 <vishal.nv21@gmail.com>
2023-10-09 14:24:32 +0530

    Favicon added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{asset('admin_css/assets/images/favicon.png')}}">
 
     <title>@yield('title',config('app.name'))</title>
 
