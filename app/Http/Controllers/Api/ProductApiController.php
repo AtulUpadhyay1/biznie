3751b7bb7ca0844967ef3f6350352c8a9c9b61f2
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-02-16 13:31:33 +0530

    product list api paginate

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -11,7 +11,8 @@ class ProductApiController extends Controller
 {
     public function index(Request $request)
     {
-        $list = HomeProduct::with('getUser', 'getCommodityProduct', 'getSellerCommodityProduct', 'getBrand')->get();
+        $list = HomeProduct::with('getUser', 'getCommodityProduct', 'getSellerCommodityProduct', 'getBrand')->paginate(getPaginate());
+        return ProductResource::collection($list);
         return response([
             'success'   => true,
             'list'      => ProductResource::collection($list)
