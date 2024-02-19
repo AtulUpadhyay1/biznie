1eeb69e13be30e771322c9171ec52625fef2d4aa
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-02-19 16:33:55 +0530

    web setup

--- app/helpers.php
+++ app/helpers.php
@@ -4,108 +4,114 @@ use App\Models\Brand;
 use App\Models\Attribute;
 use App\Models\ImageUpload;
 use App\Models\ProductUnit;
+use App\Models\WebsiteSetup;
 use App\Models\PackagingType;
 use Illuminate\Support\Facades\DB;
-    if(! function_exists('isActiveRoute')){
-        function isActiveRoute($routes=[])
-        {
-            foreach ($routes as $key => $route) {
-                if(Route::currentRouteName() == $route){
-                    return true;
-                }
+
+if (!function_exists('websiteSetupValue')) {
+    function websiteSetupValue($name){
+        return WebsiteSetup::where('name', $name)->first() ? WebsiteSetup::where('name', $name)->first()->value : "";
+    }
+}
+
+if(! function_exists('isActiveRoute')){
+    function isActiveRoute($routes=[]){
+        foreach ($routes as $key => $route) {
+            if(Route::currentRouteName() == $route){
+                return true;
             }
         }
     }
+}
 
-    if(! function_exists('getPaginate')){
-        function getPaginate($paginate = 20)
-        {
-            return $paginate;
-        }
+if(! function_exists('getPaginate')){
+    function getPaginate($paginate = 20){
+        return $paginate;
     }
+}
 
-    if(!function_exists('dateTimeFormat')){
-        function dateTimeFormat($datesTime){
-            return date('M d, Y h:i A', strtotime($datesTime));
-        }
+if(!function_exists('dateTimeFormat')){
+    function dateTimeFormat($datesTime){
+        return date('M d, Y h:i A', strtotime($datesTime));
     }
+}
 
-    if(!function_exists('dateFormat')){
-        function dateFormat($dates){
-            return date('M d, Y', strtotime($dates));
-        }
+if(!function_exists('dateFormat')){
+    function dateFormat($dates){
+        return date('M d, Y', strtotime($dates));
     }
+}
 
-    if(!function_exists('timeFormat')){
-        function timeFormat($time){
-            return date('h:i A', strtotime($time));
-        }
+if(!function_exists('timeFormat')){
+    function timeFormat($time){
+        return date('h:i A', strtotime($time));
     }
+}
 
-    if(!function_exists('lastActive')){
-        function lastActive($id){
-            $data = DB::table('personal_access_tokens')->where('tokenable_id', $id)->latest()->first();
-            $time = '';
-            if($data){
-                $time = dateTimeFormat($data->updated_at);
+if(!function_exists('lastActive')){
+    function lastActive($id){
+        $data = DB::table('personal_access_tokens')->where('tokenable_id', $id)->latest()->first();
+        $time = '';
+        if($data){
+            $time = dateTimeFormat($data->updated_at);
+        }
+        return $time;
+    }
+}
+
+if(! function_exists('imageUrl')){
+    function imageUrl($id){
+        $imageUrl="";
+        $data = ImageUpload::find($id);
+        if($data && $data->image){
+            $imageUrl = asset('storage/'.$data->image);
+        }
+        return $imageUrl;
+    }
+}
+
+if(! function_exists('imageUpload')){
+    function imageUpload($image, $folder, $id=0){
+        $data = ImageUpload::find($id);
+        if($data){
+            $image_path = 'storage/'.$data->image;
+            if(File::exists($image_path)) {
+                File::delete($image_path);
             }
-            return $time;
+        }else{
+            $data = new ImageUpload;
         }
-    }
+        $data->extension = $image->extension();
+        $image_name = time().'-'.rand(10, 99).'.'.$image->extension();
+        $data->image = $image->storeAs($folder, $image_name, 'public');
+        $data->save();
 
-    if(! function_exists('imageUrl')){
-        function imageUrl($id){
-            $imageUrl="";
-            $data = ImageUpload::find($id);
-            if($data && $data->image){
-                $imageUrl = asset('storage/'.$data->image);
-            }
-            return $imageUrl;
-        }
+        return $data->id;
     }
+}
 
-    if(! function_exists('imageUpload')){
-        function imageUpload($image, $folder, $id=0)
-        {
-            $data = ImageUpload::find($id);
-            if($data){
-                $image_path = 'storage/'.$data->image;
-                if(File::exists($image_path)) {
-                    File::delete($image_path);
-                }
-            }else{
-                $data = new ImageUpload;
-            }
-            $data->extension = $image->extension();
-            $image_name = time().'-'.rand(10, 99).'.'.$image->extension();
-            $data->image = $image->storeAs($folder, $image_name, 'public');
-            $data->save();
-
-            return $data->id;
-        }
+if(! function_exists('getAttribute')){
+    function getAttribute($id){
+        return Attribute::find($id);
     }
+}
 
-    if(! function_exists('getAttribute')){
-        function getAttribute($id){
-            return Attribute::find($id);
-        }
+if(! function_exists('getPackagingType')){
+    function getPackagingType($id){
+        return PackagingType::find($id);
     }
+}
 
-    if(! function_exists('getPackagingType')){
-        function getPackagingType($id){
-            return PackagingType::find($id);
-        }
+if(! function_exists('getBrand')){
+    function getBrand($id){
+        return Brand::find($id);
     }
+}
 
-    if(! function_exists('getBrand')){
-        function getBrand($id){
-            return Brand::find($id);
-        }
+if(! function_exists('getProductUnit')){
+    function getProductUnit($id){
+        return ProductUnit::find($id);
     }
+}
 
-    if(! function_exists('getProductUnit')){
-        function getProductUnit($id){
-            return ProductUnit::find($id);
-        }
-    }
 ?>
