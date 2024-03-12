edefe3f319ce8c309bccd2f37722c4eddaf68479
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-03-12 13:53:56 +0530

    quality section

--- resources/views/admin/commodity_product/index.blade.php
+++ resources/views/admin/commodity_product/index.blade.php
@@ -77,9 +77,9 @@
                                                 <a class="dropdown-item d-flex align-items-center" href="{{route('admin.commodity-product.edit', $data->id)}}" wire:navigate><i class="bi bi-pencil-square icon-sm me-2"></i><span>Edit</span></a>
                                                 <a class="dropdown-item d-flex align-items-center" href="{{route('admin.commodity-product.price', $data->id)}}" wire:navigate><i class="bi bi-currency-rupee icon-sm me-2"></i><span>Pricing & others</span></a>
                                                 <a class="dropdown-item d-flex align-items-center" href="{{route('admin.commodity-product.variation', $data->id)}}" wire:navigate><i class="bi bi-cart-plus icon-sm me-2"></i><span>Variation</span></a>
-                                                {{-- <a class="dropdown-item d-flex align-items-center" href="{{route('admin.commodity-product.quality', $data->id)}}" wire:navigate><i class="bi bi-tags icon-sm me-2"></i><span>Quality</span></a> --}}
+                                                <a class="dropdown-item d-flex align-items-center" href="{{route('admin.commodity-product.quality', $data->id)}}" wire:navigate><i class="bi bi-tags icon-sm me-2"></i><span>Quality</span></a>
                                                 {{-- <a href="javascript:;" class="dropdown-item d-flex align-items-center"><i class="bi bi-copy icon-sm me-2"></i><span>Duplicate</span></a>
-                                                <a href="javascript:;" class="dropdown-item d-flex align-items-center"><i class="bi bi-trash icon-sm me-2"></i><span>Delete</span></a> --}}
+                                                <a href="javascript:;" class="dropdown-item d-flex align-items-center"><i class="bi bi-trash icon-sm me-2"></i><span>Delete</span></a>--}}
                                             </div>
                                         </td>
                                     </tr>
