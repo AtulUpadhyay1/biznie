f11d29463f8394aacf087b9c337ab95242b56d60
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-05-03 15:14:08 +0530

    price calculate

--- app/Http/Resources/Customer/ProductEnquiryDetailResource.php
+++ app/Http/Resources/Customer/ProductEnquiryDetailResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Customer;
 
 use Illuminate\Http\Request;
+use App\Models\SellerCommodityProduct;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductEnquiryDetailResource extends JsonResource
@@ -38,7 +39,7 @@ class ProductEnquiryDetailResource extends JsonResource
                 ] : [],
 
             'origin_city'       => $this->origin_city,
-            'variation'         => $this->variation,
+            'variation'         => [],
             'billing_address'   => $this->billing_address,
             'delivery_address'  => $this->delivery_address,
             'consignee_detail'  => $this->consignee_detail,
@@ -46,20 +47,51 @@ class ProductEnquiryDetailResource extends JsonResource
             'description'       => $this->description,
             'message'           => $this->message,
             // 'price'             => $this->price,
+            'total_quantity'    => 0,
             'base_price'        => 0,
-            'transport_price'   => 0,
+            'loading_charge'    => 0,
+            'insurance_charge'  => 0,
+            'quality_charge'    => 0,
+            'gst'               => 0,
+            'tcs'               => 0,
+            'gst_amount'        => 0,
+            'tcs_amount'        => 0,
+            'total_charges'     => 0,
             'commission'        => 0,
+            'final_variation_price' => 0,
+            'ex_price'          => 0,
+            'transport_price'   => 0,
+            'for_price'         => 0,
             'is_mark'           => false,
             'status'            => $this->status,
         ];
+        $markedSeller = $this->getMarkedSellerProductEnquiry;
+        if($markedSeller){
 
-        if($this->getMarkedSellerProductEnquiry){
+            // $data['variation']  = $markedSeller->value;
+            $data['base_price'] = $markedSeller->base_price;
+            $data['transport_price'] = $markedSeller->transport_price;
+            $data['commission'] = $markedSeller->commission;
+            $data['is_mark']    = $markedSeller->is_mark ? true : false;
 
-            $data['variation']  = $this->getMarkedSellerProductEnquiry->value;
-            $data['base_price'] = $this->getMarkedSellerProductEnquiry->base_price;
-            $data['transport_price'] = $this->getMarkedSellerProductEnquiry->transport_price;
-            $data['commission'] = $this->getMarkedSellerProductEnquiry->commission;
-            $data['is_mark']    = $this->getMarkedSellerProductEnquiry->is_mark ? true : false;
+            foreach($markedSeller->value as $variation){
+                $variation['final_price'] = ($variation['price'] + $data['base_price']) * $variation['quantity'];
+                $data['variation'][]  = $variation;
+                $data['total_quantity'] += $variation['quantity'];
+                $data['final_variation_price'] += $variation['final_price'];
+            }
+
+            $seller_commodity_product   = SellerCommodityProduct::where('user_id', $markedSeller->user_id)->where('commodity_product_id', $markedSeller->commodity_product_id)->where('brand_id', $markedSeller->brand_id)->first();
+            $data['loading_charge']     = $seller_commodity_product->loading_charge;
+            $data['insurance_charge']   = $seller_commodity_product->insurance_charge;
+            $data['quality_charge']     = $seller_commodity_product->quality_charge ?? 0;
+            $data['gst']                = $seller_commodity_product->gst;
+            $data['tcs']                = $seller_commodity_product->tcs;
+            $data['gst_amount']         = $data['final_variation_price']*$data['gst']/100;
+            $data['tcs_amount']         = ($data['final_variation_price'] + $data['gst_amount'])*$data['tcs_amount']/100;
+
+            $data['ex_price']           = $data['final_variation_price'] + $data['gst_amount'] + $data['tcs_amount'] + $data['total_charges'];
+            $data['for_price']          = $data['ex_price'] + $data['transport_price'];
             // $data['status']     = $this->getMarkedSellerProductEnquiry->status;
         }
 
