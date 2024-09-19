13ffd577516bb06284e46011ed7e128d2f784e59
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-09-19 10:33:45 +0530

    CommodityProductOrder bug fixed

--- app/Http/Controllers/Api/Customer/Authenticated/OrderApiController.php
+++ app/Http/Controllers/Api/Customer/Authenticated/OrderApiController.php
@@ -33,7 +33,7 @@ class OrderApiController extends Controller
 
     public function show($id)
     {
-        $data = CommodityProductOrder::with('getBrand', 'getCommodityProduct', 'getCommodityProduct.getUnit', 'getCommodityProduct.getCategory', 'getDrivers')->find($id);
+        $data = CommodityProductOrder::where('customer_user_id', auth()->id())->with('getBrand', 'getCommodityProduct', 'getCommodityProduct.getUnit', 'getCommodityProduct.getCategory', 'getDrivers')->find($id);
         if(!$data){
             return response([
                 'success'   => false,
