75b374bc3be638bb7a72fc0bcfc372834d7d1a46
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2024-11-20 17:32:03 +0530

    credit availability update form admin panel

--- app/Livewire/Admin/Customer/Payments.php
+++ app/Livewire/Admin/Customer/Payments.php
@@ -13,16 +13,36 @@ class Payments extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
-    public $data;
+    public $data, $mode;
+
+    protected $queryString = [
+        'mode'    => ['except' => ''],
+    ];
+
     public function mount($id)
     {
         $this->data = User::find($id);
+        if(!$this->mode){
+            $this->mode = 'cashwallet';
+            return $this->redirectRoute('admin.customer-payment-list', ['id' => $id, 'mode' => 'cashwallet'], navigate: true);
+        }
     }
 
     public function render()
     {
-        $cash_transaction = CashWalletTransaction::where('user_id', $this->data->id)->latest()->simplePaginate(getPaginate(25));
-        $credit_transaction = CreditWalletTransaction::where('user_id', $this->data->id)->latest()->simplePaginate(getPaginate(25));
-        return view('admin.customer_list.payment', compact('cash_transaction', 'credit_transaction'), ['page_title' => 'Customer Payment List']);
+        $cash_transactions = CashWalletTransaction::where('user_id', $this->data->id)->latest()->simplePaginate(getPaginate(25));
+        $credit_transactions = CreditWalletTransaction::where('user_id', $this->data->id)->latest()->simplePaginate(getPaginate(25));
+        return view('admin.customer_list.payment', compact('cash_transactions', 'credit_transactions'), ['page_title' => 'Customer Payment List']);
+    }
+
+    public function updateCreditAvailability()
+    {
+        $user = $this->data;
+        $user->credit_availability = $user->credit_availability ? 0 : 1;
+        $user->save();
+        $this->dispatch('alert',
+            type : 'success',
+            message : 'Credit availability updated successfully.',
+        );
     }
 }
--- resources/views/admin/customer_list/payment.blade.php
+++ resources/views/admin/customer_list/payment.blade.php
@@ -16,26 +16,34 @@
                         </div>
                         <div class="col-6">
                             <div class="d-flex align-items-center justify-content-end flex-wrap text-nowrap">
-                                <button type="button" class="btn btn-danger btn-icon-text mb-2 mb-md-0">
+                                <label for="credit_availability" class="form-check-label me-1">Credit Availability</label>
+                                <div class="form-check form-switch">
+                                    <input type="checkbox" class="form-check-input status_update" id="credit_availability" wire:click="updateCreditAvailability()" {{$data->credit_availability == 1 ? 'checked' : ''}}>
+                                </div>
+                                {{-- <button type="button" class="btn btn-danger btn-icon-text mb-2 mb-md-0">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download-cloud btn-icon-prepend"><polyline points="8 17 12 21 16 17"></polyline><line x1="12" y1="12" x2="12" y2="21"></line><path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29"></path></svg>
                                     Download Report
-                                </button>
+                                </button> --}}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-6 text-end">
-                        <span class="badge border border-success text-success p-3">
-                            <h6>Cash Balance</h6>
-                            <h3> ₹ {{ formatIndianNumber($data->cash_balance) }} </h3>
-                        </span>
+                        <a href="{{route('admin.customer-payment-list', $data->id)}}?mode=cashwallet" wire:navigate>
+                            <span class="badge {{$mode == 'cashwallet' ? 'bg-success text-white' : ''}} border border-success text-success p-3">
+                                <h6>Cash Balance</h6>
+                                <h3> ₹ {{ formatIndianNumber($data->cash_balance) }} </h3>
+                            </span>
+                        </a>
                     </div>
                     <div class="col-6">
-                        <span class="badge border border-primary text-primary p-3">
-                            <h6>Credit Balance</h6>
-                            <h3> ₹ {{ formatIndianNumber($data->credit_balance) }} </h3>
-                        </span>
+                        <a href="{{route('admin.customer-payment-list', $data->id)}}?mode=creditwallet" wire:navigate>
+                            <span class="badge {{$mode == 'creditwallet' ? 'bg-primary text-white' : ''}} border border-primary text-primary p-3">
+                                <h6>Credit Balance</h6>
+                                <h3> ₹ {{ formatIndianNumber($data->credit_balance) }} </h3>
+                            </span>
+                        </a>
                     </div>
                 </div>
                 <div class="card-body">
@@ -50,14 +58,34 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>#12345674542321</td>
-                                    <td><b>RS 14000</b></td>
-                                    <td class="text-success fw-bolder">Completed</td>
-                                    <td>04/10/2023<br>6.45 PM</td>
-                                </tr>
+                                @if ($mode == 'cashwallet')
+                                    @foreach ($cash_transactions as $cash_transaction)
+                                        <tr>
+                                            <td>{{$cash_transaction->transaction_id}}</td>
+                                            <td><b>RS {{$cash_transaction->amount}}</b></td>
+                                            <td>{{ucfirst($cash_transaction->status)}}</td>
+                                            <td>{{$cash_transaction->created_at}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                                @if ($mode == 'creditwallet')
+                                    @foreach ($credit_transactions as $credit_transaction)
+                                        <tr>
+                                            <td>{{$credit_transaction->transaction_id}}</td>
+                                            <td><b>RS {{$credit_transaction->amount}}</b></td>
+                                            <td>{{ucfirst($credit_transaction->status)}}</td>
+                                            <td>{{$credit_transaction->created_at}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
                             </tbody>
                         </table>
+                        @if ($mode == 'cashwallet')
+                            {{$cash_transactions->links()}}
+                        @endif
+                        @if ($mode == 'creditwallet')
+                            {{$credit_transactions->links()}}
+                        @endif
                     </div>
                 </div>
             </div>
