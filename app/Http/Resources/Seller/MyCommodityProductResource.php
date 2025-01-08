9554b9e858c81066675131d871b4e1cf6fb00e21
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2025-01-08 17:36:00 +0530

    loading position in my-commodity-product-list api

--- app/Http/Resources/Seller/MyCommodityProductResource.php
+++ app/Http/Resources/Seller/MyCommodityProductResource.php
@@ -29,6 +29,7 @@ class MyCommodityProductResource extends JsonResource
             'unit'              => $this->getUnit ? ['id' => $this->unit_id,'name' => $this->getUnit->name] : '',
             'loading_address'   => $this->loading_address,
             'price_validity'    => $this->price_validity ? dateTimeFormat($this->price_validity) : null,
+            'loading_position'  => $this->loading_position,
             'updated_at'        => dateTimeFormat($this->updated_at),
             'brands'            => null,
             'packaging'         => [],
