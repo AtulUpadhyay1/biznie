66c315a33d06f55628f0b1a998cc1cd37b0d5f98
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2025-01-09 14:13:50 +0530

    admin dashbord

--- app/Livewire/Admin/DashboardLivewire.php
+++ app/Livewire/Admin/DashboardLivewire.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class DashboardLivewire extends Component
 {
     public function render()
     {
-        return view('admin.dashboard', ['page_title' => 'Admin Dashboard']);
+        $total_customer = User::where('type', 'customer')->count();
+        $total_seller = User::where('type', 'seller')->count();
+        $total_transporter = User::where('type', 'transporter')->count();
+        return view('admin.dashboard', compact('total_customer', 'total_seller', 'total_transporter'), ['page_title' => 'Admin Dashboard']);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,16 @@
 <div>
-    @section('title', config('app.name') . ' | '.$page_title)
+    @section('title', config('app.name') . ' | ' . $page_title)
     <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
         <div>
-            <h4 class="mb-3 mb-md-0">Welcome to Dashboard</h4>
+            <h4 class="mb-3 mb-md-0">Welcome to Your Dashboard!</h4>
         </div>
         <div class="d-flex align-items-center flex-wrap text-nowrap">
             <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
                 <span class="input-group-text input-group-addon bg-transparent border-danger" data-toggle>
                     <i class="bi bi-calendar text-danger"></i>
                 </span>
-                <input type="text" class="form-control bg-transparent border-danger" placeholder="Select date" data-input>
+                <input type="text" class="form-control bg-transparent border-danger" placeholder="Select date"
+                    data-input>
             </div>
             {{-- <button type="button" class="btn btn-outline-danger btn-icon-text me-2 mb-2 mb-md-0">
                 <i class="btn-icon-prepend" data-feather="printer"></i>
@@ -21,5 +22,91 @@
             </button>
         </div>
     </div>
-</div>
 
+    <div class="row">
+        <div class="col-12 col-xl-12 stretch-card">
+            <div class="row flex-grow-1">
+                <div class="col-md-4 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h6 class="card-title mb-0">All Buyers</h6>
+                                <div class="dropdown mb-2">
+                                    <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">
+                                        <i class="icon-lg text-secondary pb-3px bi bi-three-dots"></i>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item d-flex align-items-center" href="{{ route('admin.customer-list') }}" wire:navigate>
+                                            <i class="bi bi-eye icon-sm me-2"></i>
+                                            <span class="">View</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">{{ $total_customer }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h6 class="card-title mb-0">All Sellers</h6>
+                                <div class="dropdown mb-2">
+                                    <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">
+                                        <i class="icon-lg text-secondary pb-3px bi bi-three-dots"></i>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item d-flex align-items-center" href="{{ route('admin.seller.index') }}" wire:navigate>
+                                            <i class="bi bi-eye icon-sm me-2"></i>
+                                            <span class="">View</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">{{ $total_seller }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-baseline">
+                                <h6 class="card-title mb-0">All Transporters</h6>
+                                <div class="dropdown mb-2">
+                                    <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                                        aria-haspopup="true" aria-expanded="false">
+                                        <i class="icon-lg text-secondary pb-3px bi bi-three-dots"></i>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item d-flex align-items-center" href="{{ route('admin.transporter.index') }}" wire:navigate>
+                                            <i class="bi bi-eye icon-sm me-2"></i>
+                                            <span class="">View</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 col-md-12 col-xl-5">
+                                    <h3 class="mb-2">{{ $total_transporter }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
