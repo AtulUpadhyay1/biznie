bcde4316de37a1b2dc5d067b7aead7711b2f99b2
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2025-07-17 17:30:10 +0530

    Add refund balance functionality to Ledger component and update view

--- app/Livewire/Admin/CommodityProductOrder/Ledger.php
+++ app/Livewire/Admin/CommodityProductOrder/Ledger.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Admin\CommodityProductOrder;
 
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Models\CashWalletTransaction;
 use App\Models\CommodityProductOrder;
 use App\Models\CreditWalletTransaction;
 use App\Models\CommodityProductOrderLedger;
@@ -16,6 +17,7 @@ class Ledger extends Component
     public $page_title = 'View Order Ledger';
     public $hidden_id, $total_credit_wallet = 0, $total_pay_credit_wallet, $due_credit_wallet, $data;
     public $amount = 0, $payment_method = 'cash', $description, $notes;
+    public $mode = 'manual', $transaction_amount, $transaction_account_name, $transaction_account_number, $transaction_bank_name, $transaction_number, $refund_payment_method, $date_time, $refund_description, $file;
 
     public function mount($id)
     {
@@ -93,6 +95,114 @@ class Ledger extends Component
         $history->save();
 
         session()->flash('success', 'Credit wallet balance added successfully.');
+        return $this->redirectRoute('admin.commodity-product-order.ledger', $this->hidden_id, navigate: true);
+    }
+
+    public function addRefundBalance()
+    {
+        $order = $this->data;
+        if(!$order){
+            $this->dispatch('alert',
+                type: 'error',
+                message: 'Order not found',
+            );
+            return;
+        }
+
+        $this->validate([
+            'transaction_amount' => 'required|numeric|min:0',
+        ]);
+
+        $advance_amount = CommodityProductOrderLedger::where('order_id', $this->hidden_id)->skip(1)->first();
+        if (!$advance_amount) {
+            $this->dispatch('alert',
+                type: 'error',
+                message: 'No advance amount found for this order.',
+            );
+            return;
+        }
+        if ($this->transaction_amount > $advance_amount->amount) {
+            $this->dispatch('alert',
+                type: 'error',
+                message: 'Refund amount must be less than or equal to advance amount.',
+            );
+            return;
+        }
+
+        $user = $this->data->getCustomer;
+
+        if ($this->mode == 'credit_wallet'){
+            if ($user->assign_credit_balance == 0){
+                $this->dispatch('alert',
+                    type: 'error',
+                    message: 'Buyer does not have an active credit wallet balance.',
+                );
+                return;
+            }
+        }
+
+        $order->paid_amount -= $this->transaction_amount;
+        $order->due_amount += $this->transaction_amount;
+        $order->save();
+
+        $ledger = new CommodityProductOrderLedger;
+        $ledger->order_id = $this->hidden_id;
+        $ledger->transaction_id = "TNX-".time()."-".rand(1111, 9999);
+        $ledger->type = 'debit';
+        $ledger->amount = $this->transaction_amount;
+        $ledger->remaining_balance = $this->data->due_amount;
+        $ledger->transaction_account_name = $this->transaction_account_name;
+        $ledger->transaction_account_number = $this->transaction_account_number;
+        $ledger->transaction_bank_name = $this->transaction_bank_name;
+        $ledger->transaction_number = $this->transaction_number;
+        $ledger->payment_method = $this->payment_method;
+        $ledger->payment_mode = ucwords(str_replace('_', ' ', $this->mode));
+        $ledger->date_time = $this->date_time;
+        $ledger->description = $this->description ?? 'Amount refunded for Order Id: '.$this->data->order_id;
+        if($this->file){
+            $file_name = time().'-'.rand(10, 99).'.'.$this->file->extension();
+            $ledger->file = $this->file->storeAs('payments', $file_name, 'public');
+        }
+        $ledger->save();
+
+        if ($this->mode == 'cash_wallet'){
+
+            $user->cash_balance += $this->transaction_amount;
+            $user->save();
+
+            $cash_history = new CashWalletTransaction;
+            $cash_history->user_id           = $this->data->customer_user_id;
+            $cash_history->commodity_product_order_id   = $this->hidden_id;
+            $cash_history->amount            = $this->transaction_account_number;
+            $cash_history->description       = 'Amount refunded for Order Id: '.$this->data->order_id;
+            $cash_history->mode              = 'online';
+            $cash_history->status            = 'credit';
+            $cash_history->transaction_status= 'Amount credited';
+            $cash_history->save();
+
+            $cash_history->transaction_id    = 'TX-'.date('Ymd').$cash_history->id.$cash_history->user_id.rand(111, 999);
+            $cash_history->save();
+
+        }else if ($this->mode == 'credit_wallet'){
+
+            $user->credit_balance -= $this->transaction_amount;
+            $user->save();
+
+            $credit_history = new CreditWalletTransaction;
+            $credit_history->user_id           = $this->data->customer_user_id;
+            $credit_history->commodity_product_order_id = $this->hidden_id;
+            $credit_history->amount            = $this->transaction_amount;
+            $credit_history->description       = 'Amount refunded for Order Id: '.$this->data->order_id;
+            $credit_history->status            = 'credit';
+            $credit_history->transaction_status= 'Amount credited';
+            $credit_history->save();
+
+            $credit_history->transaction_id    = 'TX-'.date('Ymd').$credit_history->id.$credit_history->user_id.rand(111, 999);
+            $credit_history->save();
+
+        }
+
+        session()->flash('success', 'Payment refund successfully.');
         return $this->redirectRoute('admin.commodity-product-order.ledger', $this->hidden_id, navigate: true);
     }
 }
