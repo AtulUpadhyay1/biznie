6c382553b145fa8f1f528bd6d37cdf9e5e67ed97
AtulUpadhyay1 <atulupadhyay50003@gmail.com>
2025-10-14 10:22:59 +0530

    Fix pincode display logic to prioritize consignee detail over billing address

--- resources/views/admin/commodity_product_order/status.blade.php
+++ resources/views/admin/commodity_product_order/status.blade.php
@@ -68,7 +68,7 @@
                         <div class="col-6">
                             <p>
                                 <b>Billing Address</b> <br>
-                                <b>Pincode: </b> {{ $data->billing_address['pincode'] }} <br>
+                                <b>Pincode: </b> {{ isset($data->consignee_detail['pin_code']) ? $data->consignee_detail['pin_code'] : (isset($data->billing_address['pincode']) ? $data->billing_address['pincode'] : '') }} <br>
                                 <b>Address: </b> {{ $data->billing_address['address_line_one']??'' }} <br>
                                 <b>City: </b> {{ $data->billing_address['city'] }} <br>
                                 <b>State: </b> {{ $data->billing_address['state'] }} <br>
@@ -79,7 +79,7 @@
                                 <b>Consignee Detail</b> <br>
                                 <b>Company: </b> {{ $data->consignee_detail['company_name'] }} <br>
                                 <b>Phone: </b> {{ $data->consignee_detail['phone'] }} <br>
-                                <b>Pincode: </b> {{ $data->consignee_detail['pincode'] }} <br>
+                                <b>Pincode: </b> {{ isset($data->consignee_detail['pin_code']) ? $data->consignee_detail['pin_code'] : (isset($data->consignee_detail['pincode']) ? $data->consignee_detail['pincode'] : '') }} <br>
                                 <b>Address 1: </b> {{ $data->consignee_detail['address_line_one'] }} <br>
                                 <b>Address 2: </b> {{ $data->consignee_detail['address_line_two'] }} <br>
                                 <b>City: </b> {{ $data->consignee_detail['city'] }} <br>
